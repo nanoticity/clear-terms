a65e6c55dceb063e083fd58d38a65e3d3a818f82
Nano <nanoticity@gmail.com>
2026-03-03 19:49:31 -0800

    Make build script more robust

--- build.py
+++ build.py
@@ -1,7 +1,19 @@
-import shutil, os
+import shutil
+import os
 
-if os.path.exists("build"):
-    shutil.rmtree("build")
+def main() :
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(base_dir, "build")
+    extension_dir = os.path.join(base_dir, "extension")
 
-shutil.copytree("extension", "build")
-print("build complete - load the build folder into chrome")
+    if os.path.exists(build_dir):
+        if os.path.isdir(build_dir):
+            shutil.rmtree("build")
+        else:
+            os.remove(build_dir)
+
+    shutil.copytree("extension", "build")
+    print("build complete - load the build folder into chrome")
+
+if __name__ == "__main__":
+    main()
