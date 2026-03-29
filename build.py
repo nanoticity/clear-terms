579cf25668822893e93386f20acf4f100f042bf1
Nano <nanoticity@gmail.com>
2026-03-28 17:53:13 -0700

    update git and build
    Signed-off-by: Nano <nanoticity@gmail.com>

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 import shutil
 import os
 
-def main() :
+
+def main():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(base_dir, "build")
     extension_dir = os.path.join(base_dir, "extension")
@@ -14,6 +15,7 @@ def main() :
 
     shutil.copytree(extension_dir, build_dir)
     print("build complete - load the build folder into chrome")
+
 
 if __name__ == "__main__":
     main()
